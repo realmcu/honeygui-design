6be3cc6c247c90af4cafdd613900d01d70bb2414
Developer <user@example.com>
2025-11-30 21:55:10 +0800

    chore: 删除 E2E 测试生成的临时项目
    - 从 git 中删除 e2etest_test_project/
    - 添加 .gitignore 规则忽略所有测试项目
    - 测试项目应该是临时的，不应该提交到仓库

--- e2etest_test_project/.honeygui-build/win32_sim/hgconfig.h
+++
@@ -1,9 +0,0 @@
-/* Automatically generated file; DO NOT EDIT. */
-/* HoneyGUI Configuration */
-
-#ifndef HGCONFIG_H__
-#define HGCONFIG_H__
-
-#define CONFIG_REALTEK_HONEYGUI 1
-
-#endif /* HGCONFIG_H__ */
--- e2etest_test_project/src/autogen/main/E2ETestMain_callbacks.h
+++
@@ -1,8 +0,0 @@
-#ifndef E2ETESTMAIN_CALLBACKS_H
-#define E2ETESTMAIN_CALLBACKS_H
-
-#include "gui_api.h"
-
-// 事件回调函数声明
-
-#endif // E2ETESTMAIN_CALLBACKS_H
