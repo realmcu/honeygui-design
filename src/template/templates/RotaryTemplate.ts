85a334291cfdea72070b22a06b5b7fe40a4bd58d
王浩 <howie_wang@realsil.com.cn>
2025-12-12 18:13:38 +0800

    模板工程准备

---
+++ src/template/templates/RotaryTemplate.ts
@@ -0,0 +1,14 @@
+import * as path from 'path';
+import { BaseTemplate } from './BaseTemplate';
+
+export class RotaryTemplate extends BaseTemplate {
+    id = 'rotary';
+    name = 'Rotary Screen';
+    description = 'Circular UI for rotary knob devices';
+    category = 'Wearable';
+    recommendedResolution = '480X480';
+    
+    protected getTemplateProjectPath(): string {
+        return path.join(__dirname, '..', '..', '..', 'template-projects', 'rotary');
+    }
+}
---
+++ template-projects/dashboard/src/callbacks/main_callbacks.c
@@ -0,0 +1,9 @@
+#include "main_callbacks.h"
+#include "../ui/main_ui.h"
+#include <stdio.h>
+
+// 事件回调函数实现
+
+/* @protected start custom_functions */
+// 自定义函数
+/* @protected end custom_functions */
