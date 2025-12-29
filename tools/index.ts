a085d95f7bc76ab6b316d72198f0b91b35ee7d87
hellsge <hellsge@qq.com>
2025-12-29 17:28:58 +0800

    feat(tools): integrate font-converter submodule and dependencies
    Change-Id: Id293f39fcd417c0fddcdae4dfd0e6e5e24e95ac2

--- tools/index.ts
+++ tools/index.ts
@@ -7,5 +7,6 @@
 
 export { ImageConverter } from './image-converter/converter';
 export { OBJConverter } from './model-converter/obj-converter';
+export { FontGeneratorFactory as FontConverter } from './font-converter/src/main';
 export { PixelFormat } from './image-converter/types';
 export { ModelType } from './model-converter/types';
